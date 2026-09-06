68e5feba2e4ca8447f878b97c82759affdfa15f6
beatrizcostta81-creator <beatrizcostta81@gmail.com>
2026-09-06 02:22:15 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-const CACHE_NAME = 'essencia-encanto-v1';
-const APP_SHELL = [
-  './',
-  './index.html',
+const CACHE_NAME = 'essencia-encanto-v3';
+const ASSETS = [
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
@@ -12,28 +10,41 @@ const APP_SHELL = [
 self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
 
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request, { cache: 'no-store' })
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put('./index.html', copy)).catch(() => {});
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
   event.respondWith(
     fetch(event.request)
       .then(response => {
         const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {});
         return response;
       })
-      .catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
+      .catch(() => caches.match(event.request))
   );
 });
